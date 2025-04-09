c8a57a0a7ec29413c88cf74f7e9bdf703242fff8
Abhay Gupta <abhayg8900@gmail.com>
2025-04-09 12:39:10 +0530

    Updated Code

---
+++ Slip and press casting/MoldPattern.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Slip_and_press_casting
+{
+    public class MoldPattern : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public MoldPattern()
+          : base("Planarity Checker", "Pl Checker",
+        "Shows the planarity of each surface",
+        "Slip and Press Casting", "Analysis")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBrepParameter("Brep", "B", "Input Brep to analyze", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("IsPlanar", "A", "Planarity result (true/false)", GH_ParamAccess.list);
+            pManager.AddLineParameter("Normals", "B", "Face normals for visualization", GH_ParamAccess.list);
+            pManager.AddColourParameter("FaceColors", "C", "Colors based on deviation", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Deviation", "D", "Planarity deviation values", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Brep brep = null;
+            if (!DA.GetData(0, ref brep)) return;
+
+            List<bool> results = new List<bool>();
+            List<Line> normalLines = new List<Line>();
+            List<Color> faceColors = new List<Color>();
+            List<double> planarityDeviation = new List<double>();
+
+            for (int i = 0; i < brep.Faces.Count; i++)
+            {
+                BrepFace face = brep.Faces[i];
+                bool isPlanar = face.IsPlanar();
+                results.Add(isPlanar);
+
+                // Compute centroid safely
+                AreaMassProperties areaProps = AreaMassProperties.Compute(face);
+                if (areaProps == null)
+                {
+                    normalLines.Add(Line.Unset);
+                    faceColors.Add(Color.Gray);
+                    planarityDeviation.Add(0.0);
+                    continue;
+                }
+
+                Point3d centroid = areaProps.Centroid;
+                Vector3d normal = face.NormalAt(0.5, 0.5);
+                normal.Unitize();
+                Point3d normalEnd = centroid + normal * 1.0;
+                normalLines.Add(new Line(centroid, normalEnd));
+
+                // Compute planarity deviation
+                Plane bestFit;
+                double deviation = ComputePlanarityDeviation(brep, face, out bestFit);
+                planarityDeviation.Add(deviation);
+
+                Color color = deviation < 0.1 ? Color.Green :
+                              deviation < 0.5 ? Color.Yellow :
+                              Color.Red;
+                faceColors.Add(color);
+            }
+
+            DA.SetDataList(0, results);
+            DA.SetDataList(1, normalLines);
+            DA.SetDataList(2, faceColors);
+            DA.SetDataList(3, planarityDeviation);
+        }
+
+        private double ComputePlanarityDeviation(Brep brep, BrepFace face, out Plane bestFitPlane)
+        {
+            List<Point3d> controlPoints = new List<Point3d>();
+
+            foreach (int edgeIndex in face.AdjacentEdges())
+            {
+                BrepEdge edge = brep.Edges[edgeIndex];
+                Curve curve = edge.ToNurbsCurve();
+
+                if (curve != null)
+                {
+                    int sampleCount = 10;
+                    for (int i = 0; i <= sampleCount; i++)
+                    {
+                        double t = curve.Domain.ParameterAt((double)i / sampleCount);
+                        controlPoints.Add(curve.PointAt(t));
+                    }
+                }
+            }
+
+            Plane.FitPlaneToPoints(controlPoints, out bestFitPlane);
+
+            double maxDeviation = 0.0;
+            foreach (Point3d pt in controlPoints)
+            {
+                double deviation = Math.Abs(bestFitPlane.DistanceTo(pt));
+                if (deviation > maxDeviation)
+                    maxDeviation = deviation;
+            }
+
+            return maxDeviation;
+        
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("0E7E7DFC-CDEF-4AE7-948B-07BFC6580E21"); }
+        }
+    }
+}
